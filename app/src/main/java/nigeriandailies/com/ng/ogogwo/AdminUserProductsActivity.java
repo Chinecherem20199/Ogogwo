b2cf882193d05571f6d5a95c599a3c9f47e02425
chinecherem20199 <chinecherem20199@gmail.com>
2019-07-25 10:24:21 +0100

    init commit

---
+++ app/src/main/java/nigeriandailies/com/ng/ogogwo/AdminUserProductsActivity.java
@@ -0,0 +1,13 @@
+package nigeriandailies.com.ng.ogogwo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminUserProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_user_products);
+    }
+}
